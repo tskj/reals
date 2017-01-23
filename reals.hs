463d2fae6dfe8a37d76a716980693565051b7c41
epost <epost@tarjei.org>
2017-01-24 00:08:43 +0100

    Add a base system on a figure basis

--- reals.hs
+++ reals.hs
@@ -1,7 +1,21 @@
 module Reals (R(End, Point, Real)) where
 
-type Z = Integer
+data Z = Base Integer Integer
 data R = Plus R | Minus R | Real Z R | Point R | End
+
+instance Show Z where
+    show (Base x y) | y >= x = show (div y x) ++ show (mod y x)
+
+    show (Base 16 15) = "f"
+    show (Base 16 14) = "e"
+    show (Base 16 13) = "d"
+    show (Base 16 12) = "c"
+    show (Base 16 11) = "b"
+    show (Base 16 10) = "a"
+
+    show (Base x y) | x > 10 && x /= 16 = "|" ++ show y ++ "|"
+
+    show (Base x y) = show y
 
 instance Show R where
     show End        = ""
@@ -11,7 +25,7 @@ instance Show R where
     show (Minus x)  =  "-" ++ show x
 
 --instance Num R where
---   x + y = 
+--    (Point x) + (Point y) = 
 
 recNewton'sMethod f x n | n <= 0 = x
 recNewton'sMethod f x n = x2 - f x2 / f' x2
@@ -23,12 +37,12 @@ newton'sMethod' f f' x it | it <= 0 = x
 newton'sMethod' f f' x it           = newton'sMethod' f f' x2 (it-1)
     where                       x2 = x - f x / f' x
 
-newton'sMethod :: (Ord t, Fractional t) => (t -> t) (t -> t) -> t -> t -> t
+newton'sMethod :: (Ord t, Fractional t) => (t -> t) -> (t -> t) -> t -> t -> t
 newton'sMethod f f' prc x
     | abs (x2 - x) < prc = x2
     | otherwise          = newton'sMethod f f' x2 prc
     where             x2 = x - f x / f' x
 
-twelve = Plus (Real 1 (Real 2 End))
-twoAndaHalf = Plus (Real 2 (Point (Real 5 End)))
-negativePointTwo = Minus (Point (Real 2 End))
+twelve = Plus (Real (Base 10 1) (Real (Base 10 2) End))
+twoAndaHalf = Plus (Real (Base 10 2) (Point (Real (Base 10 5) End)))
+negativePointTwo = Minus (Point (Real (Base 10 2) End))
