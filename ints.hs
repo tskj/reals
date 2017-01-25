2b1d34b6a92caff86ce602a8a8361290afd5d55b
epost <epost@tarjei.org>
2017-01-25 18:59:05 +0100

    Move Z to a separate module

---
+++ ints.hs
@@ -0,0 +1,32 @@
+module Ints where
+
+data Z = Base Integer Integer
+
+instance Show Z where
+    show (Base x y) | y >= x = "|" ++ show (Base x (div y x)) ++ show (Base x (mod y x)) ++ "|"
+
+    show (Base 16 15) = "f"
+    show (Base 16 14) = "e"
+    show (Base 16 13) = "d"
+    show (Base 16 12) = "c"
+    show (Base 16 11) = "b"
+    show (Base 16 10) = "a"
+
+    show (Base x y) = show y
+
+instance Num Z where
+    Base b x + Base c y = Base d (x + y)
+        where d = max b c
+    Base b x * Base c y = Base d (x * y)
+        where d = max b c
+    negate (Base b x) = Base b (negate x)
+    abs (Base b x) = Base b (abs x)
+    signum (Base b x) = Base b (signum x)
+    fromInteger n = Base 10 n
+
+instance Eq Z where
+    Base _ x == Base _ y = x == y
+
+instance Ord Z where
+    Base _ x <= Base _ y = x <= y
+
--- reals.hs
+++ reals.hs
@@ -1,26 +1,17 @@
-module Reals (R(Positive, Negative), MajorReal(Point, (:-:)), MinorReal((:.), (:.:))) where
+module Reals    ( R(..)
+                , MajorReal(..)
+                , MinorReal(..)
+                ) where
 
-data Z = Base Integer Integer
+import Ints
+
 data R = Positive MajorReal | Negative MajorReal
 
 infixr 5 :-:
 infixr 5 :.:
 infixr 1 :.
-
 data MajorReal = Point MinorReal | (:-:) Z MajorReal
 data MinorReal = (:.) | (:.:) Z MinorReal
-
-instance Show Z where
-    show (Base x y) | y >= x = "|" ++ show (Base x (div y x)) ++ show (Base x (mod y x)) ++ "|"
-
-    show (Base 16 15) = "f"
-    show (Base 16 14) = "e"
-    show (Base 16 13) = "d"
-    show (Base 16 12) = "c"
-    show (Base 16 11) = "b"
-    show (Base 16 10) = "a"
-
-    show (Base x y) = show y
 
 instance Show R where
     show (Positive x) = show x
@@ -34,19 +25,6 @@ instance Show MajorReal where
 instance Show MinorReal where
     show (:.) = ""
     show (x :.: y) = show x ++ show y
-
-instance Num Z where
-    Base b x + Base c y = Base d (x + y)
-        where d = max b c
-    Base b x * Base c y = Base d (x * y)
-        where d = max b c
-    negate (Base b x) = Base b (negate x)
-    abs (Base b x) = Base b (abs x)
-    signum (Base b x) = Base b (signum x)
-    fromInteger n = Base 10 n
-
-instance Eq Z where
-    Base _ x == Base _ y = x == y
 
 recNewton'sMethod f x n | n <= 0 = x
 recNewton'sMethod f x n = x2 - f x2 / f' x2
