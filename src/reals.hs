eb6265a134dcef1a75b9c56c7840912cf4073e69
tarjei <tskj@windows.git>
2017-03-20 22:23:12 +0100

    Rewrite to accept Naturals instead of Ints

--- src/reals.hs
+++ src/reals.hs
@@ -15,37 +15,35 @@ data MinorReal = (:.) | (:.:) N MinorReal
 
 instance Num R where
 
-    Positive x + Positive y = Positive $ x+y -- Most significant digit might be larger than base b.
-    Positive x + Negative y = Positive $ x-y -- Needs to be handled in every one of these.
-    Negative x + Negative y = Negative $ x+y
+    Positive x + Positive y = Positive $ normalizeM . addM $ x y
+    Positive x + Negative y = Positive $ normalizeM . subM $ x y
+    Negative x + Negative y = Negative $ normalizeM . addM $ x y
     x + y = y + x
 
-    Positive x * Positive y = Positive $ x*y
-    Positive x * Negative y = Negative $ x*y
-    Negative x * Negative y = Positive $ x*y
+    Positive x * Positive y = Positive $ normalizeM . mulM $ x y
+    Positive x * Negative y = Negative $ normalizeM . mulM $ x y
+    Negative x * Negative y = Positive $ normalizeM . mulM $ x y
     x * y = y * x
 
     negate (Positive x) = Negative x
     negate (Negative x) = Positive x
 
     abs (Negative x) = Positive x
-    abs = id
+    abs (Positive x) = Positive x
 
     signum (Positive _) = Positive 1
     signum (Negative _) = Negative 1
 
     fromInteger n | n < 0  = negate . fromInteger . negate $ n
-    fromInteger n = Positive $ fromInteger (div n 10 * 10) + fromInteger (mod n 10)
+    fromInteger n = Positive $ fromInteger (div n base * base) + fromInteger (mod n base)
 
-instance Num MajorReal where
 
-    (n1 :-: Point fr1) + (n2 :-: Point fr2) = n1 + n2 + n3 :-: Point fr3
-        where fr3 = case fr1 + fr2 of
-                        (:.) -> (:.)
-                        Base b n :.: fr = Base b (mod n b) :.: fr
-              n3  = case fr1 + fr2 of
-                        (:.) -> 0
-                        Base b n :.: _ = Base b $ div n b
+normalizeM (x :-: y)
+    | x >= base = normalizeM (div' x base :-: mod' x base :-: y)
+    | otherwise = x :-: y
+
+addM (n1 :-: Point fr1) (n2 :-: Point fr2) = n1 + n2 + n3 :-: Point fr3
+
 
 instance Show R where
     show (Positive x) = show x
