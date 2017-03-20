28deeecbd25b34b955e1460ac3645b233dc80dfd
tarjei <tskj@windows.git>
2017-03-20 22:22:48 +0100

    Update Naturals test with comments

--- tests/naturals_tests.hs
+++ tests/naturals_tests.hs
@@ -7,6 +7,16 @@ one = 1::N
 two = 2::N
 five = 5::N
 
+-- The rationale for not extracting / calculating
+-- the resulting values from the basis, is that
+-- any bug could potentially affect the test as well
+
+-- This is thus a more robust test, but of course
+-- breaks if you try to use a basis other than 0..9
+-- If you try to use other digits you are on your own,
+-- the exception is something like hex which also breaks
+-- If you have a better idea, please make a pull request!
+
 test_01 = do
     assert zero Equals "0"
     assert one Equals "1"
@@ -25,7 +35,7 @@ test_01 = do
     assert (one * two) Equals "2"
     assert (one * five) Equals "5"
     assert (two * five) Equals "10"
-    assert (two * five) NotEquals "a"
+    assert (two * five) NotEquals "b"
 
 main = do
     test_01
